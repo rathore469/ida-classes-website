faf338d030a8b9a7084eac30a3aad98f871258ea
Aditya Rathore <aditya884nm@gmail.com>
2026-05-16 23:45:35 +0530

    new sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,18 +5,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://idaclassesjaipur.in",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
     },
     {
       url: "https://idaclassesjaipur.in/courses/nda",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: "https://idaclassesjaipur.in/courses/ssc-gd",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: "https://idaclassesjaipur.in/courses/airforce",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
   ];
 }
